6589e34f78b31492ba681776efc2fdff31d20fa0
Chaim <chaimeliyah@gmail.com>
2019-05-11 11:09:46 -0700

    npgsql by itself

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Npgsql;
+
+namespace stub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+var connString = "Host=postgresql;Username=postgres;Password=passw0rD!;Database=mydatabase";
+
+using (var conn = new NpgsqlConnection(connString))
+{
+    conn.Open();
+
+    // Insert some data
+    using (var cmd = new NpgsqlCommand())
+    {
+        cmd.Connection = conn;
+        cmd.CommandText = "INSERT INTO data (some_field) VALUES (@p)";
+        cmd.Parameters.AddWithValue("p", "Hello world");
+        cmd.ExecuteNonQuery();
+    }
+
+    // Retrieve all rows
+    using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+    using (var reader = cmd.ExecuteReader())
+        while (reader.Read())
+            Console.WriteLine(reader.GetString(0));
+}
+        }
+    }
+}
